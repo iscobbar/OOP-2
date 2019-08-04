2c1cde94439af0fd8e1d5cd6ebc14c0b97797dc0
İskender Gürün <iskender@hotmail.com>
2019-08-04 17:20:30 +0300

    Add project files.

---
+++ ConsoleApplication4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ex1
+            Matematik m1 = new Matematik();
+            m1.topla(3, 5);
+            //Ex2
+            Matematik.Staticstopla(4, 6);
+
+            Console.Read();
+        }
+    }
+    class Matematik
+    {
+        //Ex1
+        public void topla(int s1, int s2)
+        {
+            Console.WriteLine("Sonuç: " + (s1 + s2));
+        }
+        //Ex2
+        public static void Staticstopla(int s1, int s2)
+        {
+            Console.WriteLine("Sonuç: " + (s1 + s2));
+        }
+    }
+
+}
